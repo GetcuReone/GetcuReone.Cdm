e7fa31af1ff8c64d0409d7951b4a256328b633db
Itua Jeul <GetcuReone@hotmail.com>
2020-05-03 15:03:36 +0300

    #25 added infra

--- GetcuReone.Cdm/Infrastructure/InfrastructureTests/InfrastructureTests.cs
+++ GetcuReone.Cdm/Infrastructure/InfrastructureTests/InfrastructureTests.cs
@@ -39,7 +39,7 @@ namespace InfrastructureTests
                 var currenFolder = new DirectoryInfo(Environment.CurrentDirectory);
                 string nugetFolderPath = Path.Combine(
                     currenFolder.Parent.Parent.Parent.Parent.Parent.FullName,
-                    "GetcuReone.Cdo",
+                    "GetcuReone.Cdm",
                     "bin",
                     _buildConfiguration);
                 return new DirectoryInfo(nugetFolderPath);
@@ -65,8 +65,8 @@ namespace InfrastructureTests
                 {
                     var files = new string[]
                     {
-                        "lib/netstandard2.0/GetcuReone.Cdo.dll",
-                        "lib/netstandard2.0/GetcuReone.Cdo.xml",
+                        "lib/netstandard2.0/GetcuReone.Cdm.dll",
+                        "lib/netstandard2.0/GetcuReone.Cdm.xml",
                         "LICENSE.txt",
                         "README.md",
                     };
@@ -136,7 +136,7 @@ namespace InfrastructureTests
         public void AllNamespacesStartWithGetcuReoneTestCase()
         {
             string beginNamespace = "GetcuReone";
-            string partNameAssemblies = "GetcuReone.Cdo";
+            string partNameAssemblies = "GetcuReone.Cdm";
             string[] excludeAssemblies = new string[]
             {
             };
@@ -201,7 +201,7 @@ namespace InfrastructureTests
                 ? $"{majorVersion}.0.0.0"
                 : "1.0.0.0";
 
-            string partNameAssemblies = "GetcuReone.Cdo";
+            string partNameAssemblies = "GetcuReone.Cdm";
 
             Given("Get all file", () => InfrastructureHelper.GetAllFiles(new DirectoryInfo(Environment.CurrentDirectory).Parent.Parent.Parent.Parent.Parent))
                 .And("Get all assemblies", files => files.Where(file => file.Name.Contains(".dll")))
