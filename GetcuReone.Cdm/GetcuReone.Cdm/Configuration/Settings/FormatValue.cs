d5f4d24bbaadf2bb31d97c7a07ec71f358ccbb6c
Itua Jeul <GetcuReone@hotmail.com>
2020-05-03 15:34:31 +0300

    #25 added FormatValue

---
+++ GetcuReone.Cdm/GetcuReone.Cdm/Configuration/Settings/FormatValue.cs
@@ -0,0 +1,25 @@
+﻿namespace GetcuReone.Cdm.Configuration.Settings
+{
+    /// <summary>
+    /// Format value.
+    /// </summary>
+    public enum FormatValue
+    {
+        /// <summary>
+        /// Number.
+        /// </summary>
+        Number,
+        /// <summary>
+        /// String.
+        /// </summary>
+        String,
+        /// <summary>
+        /// Enumerate.
+        /// </summary>
+        Enumerate,
+        /// <summary>
+        /// Enumerate case sensitive.
+        /// </summary>
+        EnumerateMatchCase
+    }
+}
