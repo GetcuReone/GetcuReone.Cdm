d52243b764d72a2e6b1f195e64d149fbc3f88c38
Itua Jeul <GetcuReone@hotmail.com>
2020-05-10 15:24:42 +0300

    #25 added erros

---
+++ GetcuReone.Cdm/GetcuReone.Cdm/Errors/GetcuReoneException.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace GetcuReone.Cdm.Errors
+{
+    /// <summary>
+    /// Common exception for GR systems
+    /// </summary>
+    public class GetcuReoneException : Exception
+    {
+        /// <summary>
+        /// Additional error information.
+        /// </summary>
+        public IReadOnlyCollection<ErrorDetail> ErrorDetails { get; }
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        /// <param name="errorDetails"></param>
+        public GetcuReoneException(IReadOnlyCollection<ErrorDetail> errorDetails)
+            : base(errorDetails == null || errorDetails.Count == 0 ? "Unknown error" : errorDetails.First().Reason)
+        {
+            ErrorDetails = errorDetails;
+        }
+    }
+}
