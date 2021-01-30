f619cbfe95dfbdb262d980bb34869a5fbcabadcb
Juel <GetcuReone@hotmail.com>
2021-01-30 20:08:36 +0300

    Add entities for configuration Fixes #32

---
+++ GetcuReone.Cdm/GetcuReone.Cdm/Configuration/GetcuReoneConfig.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Xml.Serialization;
+
+namespace GetcuReone.Cdm.Configuration
+{
+    /// <summary>
+    /// GetcuReone config.
+    /// </summary>
+    [Serializable]
+    public sealed class GetcuReoneConfig
+    {
+        /// <summary>
+        /// Settings configurations.
+        /// </summary>
+        [XmlElement(ElementName = "settingsConfig")]
+        public List<ConfigValue> SettingsConfigs { get; set; }
+    }
+}
