ecc040e33b0f21888723fc575ef6b1bf689158cd
Juel <GetcuReone@hotmail.com>
2021-03-01 14:58:36 +0300

    There is no way to change to add your own settings Fixes

---
+++ GetcuReone.Cdm/GetcuReone.Cdm/Configuration/SectionConfig.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Xml.Serialization;
+
+namespace GetcuReone.Cdm.Configuration
+{
+    /// <summary>
+    /// Section config
+    /// </summary>
+    [Serializable]
+    public class SectionConfig
+    {
+        /// <summary>
+        /// Setting name.
+        /// </summary>
+        [XmlAttribute(AttributeName = "name")]
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Logging configurations.
+        /// </summary>
+        [XmlElement(ElementName = "config")]
+        public List<ConfigValue> Configs { get; set; }
+    }
+}
